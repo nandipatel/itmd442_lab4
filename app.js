5206a9962cb5e4b9c54a5cfeb08e0b7380771827
Nandi Patel <npatel127@hawk.iit.edu>
2023-04-08 03:57:01 -0500

    got the /addname to add to mongo

--- app.js
+++ app.js
@@ -6,36 +6,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+const conn_str = 'mongodb+srv://npatel127:npatel127@cluster0.hzj7ktr.mongodb.net/contactdb?retryWrites=true&w=majority'
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://npatel127:npatel127@cluster0.hzj7ktr.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(conn_str);
+        console.log(`Mongo db connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+module.exports = connectDB;
+connectDB();
 
-//mongoose.connect("mongodb://localhost:27017/contact_db");
-var nameSchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email_add: String,
     notes: String,
-});
-var User = mongoose.model("User", nameSchema);
+    }, {timestamps: true});
+
+var User = mongoose.model("users", userSchema);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
-app.get("/", (req, res) => {
+app.get("/add", (req, res) => {
     res.sendFile(__dirname + "/add.html");
 });
-app.get("/", (req, res) => {
+app.get("/delete", (req, res) => {
     res.sendFile(__dirname + "/delete.html");
 });
 
+//add user
 app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
@@ -45,6 +49,16 @@ app.post("/addname", (req, res) => {
         .catch(err => {
             res.status(400).send("Unable to save to database");
         });
+});
+
+//search user
+app.post("/searchname", async (req, res) => {
+    let collection = await db.collection("users");
+    let query = {_id: ObjectId(req.params.id)};
+    let result = await collection.find(query);
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
 });
 
 app.listen(port, () => {
